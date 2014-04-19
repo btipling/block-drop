1a754325d487a6d1bad17bed6c08efe3ca798e7b
Bjorn Tipling <bjorn@ambientchill.com>
2014-04-19 09:16:19 -0700

    Board improvements.

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 /**
@@ -8,20 +9,26 @@ public class Board {
     private JButton stopButton;
     private JButton startButton;
     private JButton pauseButton;
-    private JPanel gameBoard;
+    private JPanel gamePanel;
     private JPanel controlPanel;
     private JLabel currentScore;
     private JLabel topScore;
     private JPanel nextPiecePanel;
     private JPanel contentPanel;
+    private JTextArea useArrowKeysToTextArea;
 
     public void showBoard () {
         JFrame frame = new JFrame("Drop Block");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.getContentPane().add(contentPanel);
-        frame.setMinimumSize(new Dimension(750, 500));
+        frame.setMinimumSize(new Dimension(520, 600));
         frame.setLocationRelativeTo(null);
         frame.pack();
+        controlPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
         frame.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        gamePanel = new GamePanel();
     }
 }
